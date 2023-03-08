2524e94200efc0db713b021cab76f6e0394bee6b
Liza Zhukova <Elishkaaaa@yandex.ru>
2023-03-08 13:23:24 +0300

    fix: исправлены недочеты

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req, res, next) => {
   req.user = {
